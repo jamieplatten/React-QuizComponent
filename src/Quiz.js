941c19dd78e8fc4823587bbbb249630f6fffe0ff
Jamie Platten <jamie.platten@uea.ac.uk>
2021-09-23 21:20:20 +0100

    Add quiz end component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
 
 let quizData = require("./quiz_data.json");
+
 class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 };
     }
+
     render() {
+        const isQuizEnd = quizData.quiz_questions.length === this.state;
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
+                {isQuizEnd ? (
+                    <QuizEnd />
+                ) : (
+                    <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
+                )}
             </div>
         );
     }
