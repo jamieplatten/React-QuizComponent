1e1328ecc94a81a2682b820adb2c49b8af127acd
Jamie Platten <jamie.platten@uea.ac.uk>
2021-09-23 20:48:32 +0100

    Add quiz question

---
+++ src/QuizQuestion.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { quiz_position: -1 };
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <li>{this.props.quiz_question.answer_options}</li>
+                    </ul>
+                </section>
+            </main>
+        );
+    }
+}
+
+export default QuizQuestion;
